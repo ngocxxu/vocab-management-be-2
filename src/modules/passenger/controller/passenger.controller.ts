f07d1bd489daba2413fa411741c9a84dbf7d6bdc
Ngoc <ngocquach97@gmail.com>
2025-06-03 13:12:43 +0700

    feat: add prettier

--- src/modules/passenger/controller/passenger.controller.ts
+++ src/modules/passenger/controller/passenger.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, HttpStatus, Inject, Post, PreconditionFailedException, UseGuards } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    HttpStatus,
+    Inject,
+    Post,
+    PreconditionFailedException,
+    UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { Config, LoggerService, RestrictedGuard } from '../../common';
@@ -12,19 +21,17 @@ import { PassengerService } from '../service';
 @ApiTags('passenger')
 @ApiBearerAuth()
 export class PassengerController {
-
     public constructor(
         @Inject(Service.CONFIG)
         private readonly config: Config,
         private readonly logger: LoggerService,
-        private readonly passengerService: PassengerService
-    ) { }
+        private readonly passengerService: PassengerService,
+    ) {}
 
     @Get()
     @ApiOperation({ summary: 'Find passengers' })
     @ApiResponse({ status: HttpStatus.OK, isArray: true, type: PassengerData })
     public async find(): Promise<PassengerData[]> {
-
         return this.passengerService.find();
     }
 
@@ -33,7 +40,6 @@ export class PassengerController {
     @ApiOperation({ summary: 'Create passenger' })
     @ApiResponse({ status: HttpStatus.CREATED, type: PassengerData })
     public async create(@Body(PassengerPipe) input: PassengerInput): Promise<PassengerData> {
-
         if (this.config.PASSENGERS_ALLOWED === 'no') {
             throw new PreconditionFailedException('Not allowed to onboard passengers');
         }
@@ -43,5 +49,4 @@ export class PassengerController {
 
         return passenger;
     }
-
 }
