b1c0581faa86a2fc4f649e272d22088244aed4d5
bono-ste <phungbaongoc.quach@stengg.com>
2025-07-17 10:28:22 +0700

    feat: change gmail

--- src/modules/email/service/email.service.ts
+++ src/modules/email/service/email.service.ts
@@ -8,9 +8,7 @@ export class EmailService {
 
   public constructor() {
     this.transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: Number(process.env.EMAIL_PORT),
-      secure: false,
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
