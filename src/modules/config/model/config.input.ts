47150e889ec213c0fa245907943e59eb30a991f8
ngocxxu <ngocquach43@gmail.com>
2025-11-09 01:11:26 +0700

    fix: update example value in ConfigInput to a single string

--- src/modules/config/model/config.input.ts
+++ src/modules/config/model/config.input.ts
@@ -4,8 +4,7 @@ import { Prisma } from '@prisma/client';
 export class ConfigInput {
     @ApiProperty({
         description: 'Config value (JSON)',
-        example: ['gemini-2.0-flash-lite', 'gemini-2.0-flash'],
+        example: 'gemini-2.0-flash-lite',
     })
     public readonly value: Prisma.InputJsonValue;
 }
-
